662fcd984b6731796ce022b19cd3c94e8d3d35ec
antoniocarrillo471 <araragiantonio12@gmail.com>
2020-10-02 12:14:35 -0500

    version 3

---
+++ contenido.php
@@ -0,0 +1,20 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/bakemonogatari.jpg"."||".
+				  "bakemonogatari"."||".
+				  "Bakemonogatari se centra en Koyomi Araragi, un estudiante de tercer año de instituto que es ahora casi humano después de haber sido vampiro por un tiempo. Un día, una compañera de clase llamada Hitagi Senjōgahara, quien nunca habla con nadie, cae desde una alta escalera en los brazos de Koyomi. Él descubre que Hitagi no pesa casi nada, una contradicción física. A pesar de ser amenazado por ella, Araragi le ofrece su ayuda, y la presenta a Meme Oshino, un hombre excéntrico de mediana edad viviendo en un edificio abandonado que le ayudó a dejar de ser un vampiro.";
+
+		$datos[1]="img/kami.jpg"."||".
+				  "kami nomi zo shiru sekai"."||".
+				  " La historia se centra en Keima Katsuragi, un estudiante de preparatoria muy bueno en los videojuegos bishōjo, quien acepta por error un contrato para convertirse en un colaborador para atrapar espíritus prófugos del infierno Keima ahora deberá utilizar su capacidad de análisis y experiencias en los juegos para conquistar -y así liberar- a aquellas chicas que sean contenedores de los espíritus";
+
+		$datos[2]="img/tokyoghoul.png"."||".
+				  "tokyo ghoul"."||".
+				  "Tokio se ha convertido en una ciudad cruel y despiadada, un lugar donde criaturas crueles llamadas demonios coexisten junto a los humanos. Los ciudadanos de esta gran metrópolis viven en constante temor de estos sedientos seres salvajes con un enorme apetito por la carne humana.";
+		return $datos;
+	}
+
+ ?>
